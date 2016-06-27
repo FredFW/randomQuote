222b11ff4813119ecc9ac26a610d811deafb1369
Fred Wong <fai.wong@aggiemail.usu.edu>
2016-06-27 13:22:36 +0800

    Add files via upload

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,48 +1,81 @@
-function aj (){
+function aj(){
 
-  var xhttp;
+  
+var xhttp;
 
-  if (window.XMLHttpRequest){
-    xhttp = new XMLHttpRequest();
-  }
-  else{
+  
+if (window.XMLHttpRequest)
+    {
+xhttp = new XMLHttpRequest();
+  
+}
+else{
+
     xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
   }
+
 
   xhttp.onreadystatechange = function (){
-    if (xhttp.readyState == 4 && xhttp.status == 200){
-      document.getElementById("demo").innerHTML = xhttp.responseText;
-    }
-  };
 
+    if (xhttp.readyState == 4 && xhttp.status == 200){
+
+      document.getElementById("demo").innerHTML = xhttp.responseText;
+
+    }
+};
+
+  
   xhttp.open("GET","test.txt?t=" + Math.random(),true);
+
   xhttp.send();
 
 }
 
+
+
 function parseQuote(response)
-		{
-			document.getElementById("quote").innerHTML = response.quoteText;
-		}
+{
+
+  document.getElementById("quote").innerHTML = response.quoteText;
+
+}
+
+
 
 
 function getQuote(){
+
   var xhttp;
 
+
   if (window.XMLHttpRequest){
+
     xhttp = new XMLHttpRequest();
-  }
-  else{
-    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
   }
 
+  else{
+
+    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+  
+  }
+
+  
   xhttp.onreadystatechange = function (){
-    if (xhttp.readyState == 4 && xhttp.status == 200){
-      document.getElementById("quoteText").innerHTML = xhttp.responseText;
-    }
+
+  if (xhttp.readyState == 4 && xhttp.status == 200){
+
+    document.getElementById("quoteText").innerHTML = xhttp.responseText;
+
+  }
+  
   };
 
+  
   xhttp.open("GET","http://api.forismatic.com/api/1.0/?method=getQuote&format=jsonp&jsonp=parseQuote",true);
+
   xhttp.send();
+
 
 }
