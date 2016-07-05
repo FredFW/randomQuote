7333dedd44950c323ce84dfeb682dfe19ddf1b44
Fred Wong <fai.wong@aggiemail.usu.edu>
2016-07-05 11:57:34 +0800

    Update main.js

--- Scripts/main.js
+++ Scripts/main.js
@@ -82,7 +82,7 @@ function getQuote(){
 
 function parseQuote(response){
   
-  document.getElementsByTagName("BODY").background-color === "lightblue" ? document.getElementsByTagName("BODY").background-color = "lightgreen" : document.getElementsByTagName("BODY").background-color = "orange";
+  document.getElementsByTagName("BODY").style.backgroundColor == "lightblue" ? document.getElementsByTagName("BODY").style.backgroundColor = "lightgreen" : document.getElementsByTagName("BODY").style.backgroundColor = "orange";
   
   var quotes = new RegExp('\"',"g");
   
